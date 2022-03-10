28bff23a08b2c8d13627d3c8f894d09b07cb46d9
Kiran Randhawa <4026553+FacePalmDev@users.noreply.github.com>
2022-03-10 12:17:43 +0000

    Update ExceptionExtensions.cs
    Fixed code indentation

--- CSharp/ExceptionExtensions.cs
+++ CSharp/ExceptionExtensions.cs
@@ -1,47 +1,47 @@
-   public static class ExceptionExtensions
-    {
-        /// <summary>
-        /// Check any object for null and throw exception if so.
-        /// </summary>
-        /// <param name="obj">The object to check</param>
-        /// <param name="message">The error message</param>
-        /// <exception cref="ArgumentNullException"></exception>
-        public static void ThrowIfNull(this object obj, string message = null)
-        {
-            if (obj == null)
-            {
-                throw new ArgumentNullException(message);
-            }
-        }
+public static class ExceptionExtensions
+{
+  /// <summary>
+  /// Check any object for null and throw exception if so.
+  /// </summary>
+  /// <param name="obj">The object to check</param>
+  /// <param name="message">The error message</param>
+  /// <exception cref="ArgumentNullException"></exception>
+  public static void ThrowIfNull(this object obj, string message = null)
+  {
+      if (obj == null)
+      {
+          throw new ArgumentNullException(message);
+      }
+  }
 
-        /// <summary>
-        /// Check any object for null and throw exception if so.
-        /// </summary>
-        /// <param name="obj">The object to check</param>
-        /// <param name="message">The error message</param>
-        /// <exception cref="ArgumentNullException">Throws if null</exception>
-        /// <exception cref="EmptyValueException">Throws if empty</exception>
-        public static void ThrowIfNullOrEmpty(this Array array, string message = null)
-        {
-            array.ThrowIfNull();
-            if (array.Length == 0)
-            {
-                throw new EmptyValueException(message);
-            }
-        }
+  /// <summary>
+  /// Check any object for null and throw exception if so.
+  /// </summary>
+  /// <param name="obj">The object to check</param>
+  /// <param name="message">The error message</param>
+  /// <exception cref="ArgumentNullException">Throws if null</exception>
+  /// <exception cref="EmptyValueException">Throws if empty</exception>
+  public static void ThrowIfNullOrEmpty(this Array array, string message = null)
+  {
+      array.ThrowIfNull();
+      if (array.Length == 0)
+      {
+          throw new EmptyValueException(message);
+      }
+  }
 
-        /// <summary>
-        /// Check for null or empty and throw exception if so.
-        /// </summary>
-        /// <param name="obj">The object to check</param>
-        /// <exception cref="ArgumentNullException">Throws if null</exception>
-        /// <exception cref="EmptyValueException">Throws if empty</exception>
-        public static void ThrowIfNullOrEmpty<T>(this IEnumerable<T> collection, string message = null)
-        {
-            collection.ThrowIfNull();
-            if (collection.Any() == false)
-            {
-                throw new EmptyValueException(message);
-            }
-        }
-    }
+  /// <summary>
+  /// Check for null or empty and throw exception if so.
+  /// </summary>
+  /// <param name="obj">The object to check</param>
+  /// <exception cref="ArgumentNullException">Throws if null</exception>
+  /// <exception cref="EmptyValueException">Throws if empty</exception>
+  public static void ThrowIfNullOrEmpty<T>(this IEnumerable<T> collection, string message = null)
+  {
+      collection.ThrowIfNull();
+      if (collection.Any() == false)
+      {
+          throw new EmptyValueException(message);
+      }
+  }
+}
