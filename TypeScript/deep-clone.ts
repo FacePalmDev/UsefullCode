f5e37d7417c9dc90aadbceb5e50b88cd9cac07f9
Kiran Randhawa <4026553+FacePalmDev@users.noreply.github.com>
2022-11-30 12:25:38 +0000

    Create deep-clone.ts

---
+++ TypeScript/deep-clone.ts
@@ -0,0 +1,20 @@
+/* Spread { ... } will only shallow clone objects. If those objects have properties then they'll be treated as a reference.
+One approach is to use JSON.parse(JSON.stringify(thing), but this is not performant and frankly it's ewwww... 
+The code below offers a better solution to this problem. */
+
+interface IClonable {
+  clone(): object;
+}
+
+(Object.prototype as any).clone = function (): Record<string, any> {
+  const item = this as Record<string, any>
+  const clone = { ...item };
+  //? item
+  const keys = Object.keys(item);
+  for (const key of keys) {
+    const property = item[key];
+    clone[key] = property instanceof Object ? { ...property } : property;
+  }
+  return clone;
+}
+
