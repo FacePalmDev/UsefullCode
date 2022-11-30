39972d1430cb2ce2fccead0825c3e9b87f019ca8
Kiran Randhawa <4026553+FacePalmDev@users.noreply.github.com>
2022-11-30 12:49:26 +0000

    Update deep-clone.ts
    Now using a decorator class instead of an extension method.

--- TypeScript/deep-clone.ts
+++ TypeScript/deep-clone.ts
@@ -2,17 +2,25 @@
 One approach is to use JSON.parse(JSON.stringify(thing), but this is not performant and frankly it's ewwww... 
 The code below offers a better solution to this problem. */
 
-interface IClonable {
-  clone(): object;
+export interface IDeepClonable {
+  deepClone(): object;
 }
-
-(Object.prototype as any).clone = function (): Record<string, any> {
-  const item = this as Record<string, any>;
-  const clone = { ...item };
-  const keys = Object.keys(item);
-  for (const key of keys) {
-    const property = item[key];
-    clone[key] = property instanceof Object ? { ...property } : property;
+export class DeepClonable extends Object implements IDeepClonable {
+  #obj: Record<string, any>;
+  // eslint-disable-next-line @typescript-eslint/explicit-module-boundary-types
+  constructor(objectToClone: any) {
+    super();
+    this.#obj = objectToClone;
   }
-  return clone;
-};
+
+  deepClone(): object {
+    const item = this as Record<string, any>;
+    const clone = { ...item };
+    const keys = Object.keys(item);
+    for (const key of keys) {
+      const property = item[key];
+      clone[key] = property instanceof Object ? { ...property } : property;
+    }
+    return clone;
+  }
+}
